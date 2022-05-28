b4c3504bd3fd299208cb93df76f87649703e8a51
Buckimus <ryan42buck@gmail.com>
2022-05-28 14:45:32 -0400

    created name and player name variables

---
+++ assets/js/game.js
@@ -0,0 +1,7 @@
+var playerName = window.prompt("What is your robot's name?");
+window.alert(playerName);
+
+console.log(playerName);
+console.log("This logs a string, good for leaving yourself a message");
+console.log(10 + 10);
+console.log("Our robot's name is " + playerName);
